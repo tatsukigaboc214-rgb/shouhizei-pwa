e5a9e25df35c6c479d4c1e6a0a94a06d71996673
tatsukigaboc214-rgb <tatsuki.gaboc.214@gmail.com>
2026-06-25 01:43:01 +0900

    update routing for mode selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import QuestionMenu from './pages/QuestionMenu'
 import PillarMode from './pages/PillarMode'
+import BodyMode from './pages/BodyMode'
+import AudioMode from './pages/AudioMode'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/question/:id" element={<PillarMode />} />
+        <Route path="/question/:id" element={<QuestionMenu />} />
+        <Route path="/question/:id/pillar" element={<PillarMode />} />
+        <Route path="/question/:id/body" element={<BodyMode />} />
+        <Route path="/question/:id/audio" element={<AudioMode />} />
       </Routes>
     </BrowserRouter>
   )
